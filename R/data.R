09b9397c2bfc6d31e5a974113ac9001bcd5fb599
Ross Jacobucci <rcjacobuc@gmail.com>
2017-10-01 17:04:26 -0400

    Everything checked

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Longitudinal data with groups
+#'
+#' A dataset used as an example for longRPart2
+#'
+#' @format A data frame with 600 rows and 4 variables:
+#' \describe{
+#'   \item{id}{id number}
+#'   \item{z}{covariate}
+#'   \item{time}{time variable}
+#'   \item{y}{outcome}
+#'   ...
+#' }
+"ex.data.3"
--- R/lrp.R
+++ R/lrp.R
@@ -29,9 +29,29 @@
 #' @importFrom stats formula lm na.omit predict quantile rnorm time nls terms
 #' @export
 #' @examples
+#' \dontrun{
 #' library(longRPart2)
-#' # example goes here
-
+#' data(ex.data.3)
+#' model.0 = nlme(y~b0i+b1i*time,
+#'                data=ex.data.3,
+#'                fixed=b0i+b1i~1,
+#'                random=b0i+b1i~1,
+#'                group=~id,
+#'                start=c(10,5))
+#'
+#'
+#'lcart.mod1 <- lrp(method="nlme",
+#'                  nlme.model=y~b0i+b1i*time,
+#'                  fixedFormula=b0i+b1i~1,
+#'                  rPartFormula = ~ z,
+#'                  group= ~ id,
+#'                  randomFormula=b0i+b1i~1,
+#'                  data=ex.data.3,
+#'                  start=c(10,5))
+#'summary(lcart.mod1)
+#'plot(lcart.mod1)
+#'lrp2Plot(lcart.mod1)
+#'}
 
 lrp <- function(method,
                        nlme.model=NULL,
