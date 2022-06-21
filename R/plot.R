819fc1134b75ce71c235c320d99ab3d0fc4d9883
Ross Jacobucci <rcjacobuc@gmail.com>
2022-06-21 09:50:55 -0400

    Update plot.R

--- R/plot.R
+++ R/plot.R
@@ -1,11 +1,12 @@
 #' Plot function for longRPart2
 #'
 #' @param x A model from lrp.
+#' @param box.palette Color scheme for rpart.plot
 #' @param ... Other arguments.
 #' @method plot lrp
 #' @export
 
-plot.lrp <- function (x, ...){
+plot.lrp <- function (x,box.palette="auto",...){
 
   rp = x$rpart_out
 
@@ -14,5 +15,5 @@ plot.lrp <- function (x, ...){
     paste("dev", round(summary(x)$frame$dev,2))
   }
 
-  rpart.plot::rpart.plot(rp,nn=T,node.fun=node.fun1)
+  rpart.plot::rpart.plot(rp,nn=T,box.palette=box.palette,node.fun=node.fun1)
 }
